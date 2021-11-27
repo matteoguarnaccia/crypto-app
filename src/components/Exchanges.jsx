9764db7ef1e541b3c9eb369f126801ff1a806b70
M2A1G <matteoguarnaccia@outlook.it>
2021-11-27 19:53:54 +0100

    6. Exchnges component

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import { Col, Row, Collapse, Spin, Typography, Avatar } from "antd";
+import { useGetCryptoExchangesQuery } from "../services/cryptoApi";
+import millify from "millify";
+import HTMLReactParser from "html-react-parser";
+
+const { Title, Text } = Typography;
+const { Panel } = Collapse;
 
 const Exchanges = () => {
-  return <div>Exchanges</div>;
+  const { data, isFetching } = useGetCryptoExchangesQuery();
+  console.log(data);
+
+  if (isFetching)
+    return (
+      <div className="loader">
+        <Spin />
+      </div>
+    );
+
+  return (
+    <>
+      <Row>
+        <Col span={6}>
+          <Title level={5}>Exchanges</Title>
+        </Col>
+        <Col span={6}>
+          <Title level={5}>24h Trade Volume</Title>
+        </Col>
+        <Col span={6}>
+          <Title level={5}>Market</Title>
+        </Col>
+        <Col span={6}>
+          <Title level={5}>Change</Title>
+        </Col>
+      </Row>
+      <Collapse defaultActiveKey={["1"]}>
+        {data?.data?.exchanges?.map((exchange) => (
+          <Panel
+            showArrow={false}
+            key={exchange.id}
+            header={
+              <>
+                <Col span={6}>
+                  <Title level={5}>
+                    {exchange.rank}.
+                    <Avatar className="exchange-image" src={exchange.iconUrl} />
+                    {exchange.name}
+                  </Title>
+                </Col>
+                <Col span={6}>
+                  <Text>{millify(exchange.volume)}</Text>
+                </Col>
+                <Col span={6}>
+                  <Text level={5}>{millify(exchange.numberOfMarkets)}</Text>
+                </Col>
+                <Col span={6}>
+                  <Text level={5}>{millify(exchange.marketShare)}</Text>
+                </Col>
+              </>
+            }
+          >
+            {HTMLReactParser(exchange.description || "")}
+          </Panel>
+        ))}
+      </Collapse>
+    </>
+  );
 };
 
 export default Exchanges;
